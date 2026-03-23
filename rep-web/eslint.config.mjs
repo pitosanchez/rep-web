bf3ba651f7ed33e8bc68598e78b8833825699033
Robert Pito Sanchez <robsanchez124@gmail.com>
2026-03-23 01:45:07 -0400

    changes to file

--- rep-web/eslint.config.mjs
+++ rep-web/eslint.config.mjs
@@ -5,16 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-
+  // Override default ignores of eslint-config-next.
   globalIgnores([
+    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
-
+  // Disable explicit any rule for dynamic data types
   {
-    files: ["**/*.ts", "**/*.tsx"],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
     },
